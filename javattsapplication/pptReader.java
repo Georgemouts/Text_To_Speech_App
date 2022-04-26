34988e045b1e1836dcb5aaccffbaec4dc631ec65
Giorgos Moutsopoulos <georgemuts@gmail.com>
2022-04-26 14:41:09 +0300

    Add files via upload

---
+++ javattsapplication/pptReader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javattsapplication;
+
+/**
+ *
+ * @author Ammar Mansab
+ */
+public class pptReader implements DocumentReader
+{
+    @Override
+    public void read()
+    {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
